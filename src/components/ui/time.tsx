1e492b4fb8db3aa6c49b8dd1ad2924fcdbed0240
unknown <zxckendrick123@gmail.com>
2024-07-04 15:23:36 +0800

    12 hour time

--- src/components/ui/time.tsx
+++ src/components/ui/time.tsx
@@ -3,18 +3,26 @@ import React, { useState, useEffect } from 'react';
 const Time: React.FC = () => {
   const [showTime, setShowTime] = useState<string>("");
     
-    useEffect(() => {
-        const updateTime = () => {
-            const date = new Date();
-            const timeString = date.getHours() 
-                + ':' + date.getMinutes() 
-                + ":" + date.getSeconds();
-            setShowTime(timeString);
-        };
+  useEffect(() => {
+    const updateTime = () => {
+        const date = new Date();
+        let hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
 
-        updateTime();
-        const intervalId = setInterval(updateTime, 1000); 
-        return () => clearInterval(intervalId); 
+        hours = hours % 12;
+        hours = hours ? hours : 12; 
+
+        const formatTime = (num: number) => num.toString().padStart(2, '0');
+
+        const timeString = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)} ${ampm}`;
+        setShowTime(timeString);
+    };
+
+      updateTime();
+      const intervalId = setInterval(updateTime, 1000); 
+      return () => clearInterval(intervalId); 
     }, []);
   return (
     <div className="font-bold">
